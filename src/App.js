d2a302558651c34c186362466304f5dbd6f9eee4
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-02-23 10:18:31 +0200

    rolling dice func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import PlayerDisplay from './cmps/PlayerDisplay/PlayerDisplay';
 import './App.css';
 
 function App() {
+  const [totalScore, setTotalScore] = useState({player1: 0, player2: 0})
+  const [currentScore, setCurrentScore] = useState(0)
+  const [dice, setDice] = useState({dice1: '', dice2: ''})
+
+  const rollingDice = () =>{
+    const dice1 = Math.floor(Math.random() * 6)+1
+    const dice2 = Math.floor(Math.random() * 6)+1
+    console.log(dice1, dice2);
+    setDice({dice1, dice2})
+    setCurrentScore(prevState=>prevState+dice1+dice2)
+  }
+
+  console.log(dice, currentScore);
   return (
     <div className="App">
      <PlayerDisplay header={'player 2'}/>
@@ -11,7 +25,7 @@ function App() {
         <div><img alt='' src=''/></div>
         <div><img alt='' src=''/></div>
       </div>
-      <div>Roll Dice</div>
+      <div onClick={rollingDice}>Roll Dice</div>
       <div>HOLD</div>
       <label>Score to win: <input/></label>
      </div>
