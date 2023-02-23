824150092ab9f334ab2fc506e2b54b05c6cf44b8
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-02-23 10:43:08 +0200

    reset game func

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   const rollingDice = () =>{
     const dice1 = Math.floor(Math.random() * 6)+1
     const dice2 = Math.floor(Math.random() * 6)+1
-    console.log(dice1, dice2);
     setDice({dice1, dice2})
     if(dice1 === 6 && dice2 === 6) switchTurn()
     else setCurrentScore(prevState=>prevState+dice1+dice2)
@@ -30,12 +29,19 @@ function App() {
     setCurrTurn(prevState=>prevState==='player1'?'player2':'player1')
   }
 
+  const resetGame = () => {
+    setScorePlayer1(0)
+    setScorePlayer2(0)
+    setCurrentScore(0)
+    setCurrTurn('player1')
+  }
+
   console.log(dice, currentScore, scorePlayer1, scorePlayer2);
   return (
     <div className="App">
      <PlayerDisplay header={'player 2'}/>
      <div className='board'>
-      <div>new game</div>
+      <div onClick={resetGame}>new game</div>
       <div>
         <div><img alt='' src=''/></div>
         <div><img alt='' src=''/></div>
