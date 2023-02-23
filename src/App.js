df9710c8f4f1e851313d47602b853c4c84e8f5b6
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-02-23 10:03:01 +0200

    some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import PlayerDisplay from './cmps/PlayerDisplay/PlayerDisplay';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     hello
+     <PlayerDisplay header={'player 2'}/>
+     <div className='board'>
+      <div>new game</div>
+      <div>
+        <div><img alt='' src=''/></div>
+        <div><img alt='' src=''/></div>
+      </div>
+      <div>Roll Dice</div>
+      <div>HOLD</div>
+      <label>Score to win: <input/></label>
+     </div>
+     <PlayerDisplay header={'player 1'}/>
     </div>
   );
 }
--- src/cmps/PlayerDisplay/PlayerDisplay.js
+++ src/cmps/PlayerDisplay/PlayerDisplay.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './player-display.css'
 
-const PlayerDisplay = () => {
+const PlayerDisplay = ({header}) => {
   return (
-    <div className='player-display'>
-        player 1
+    <div className='player-display' style={header==='player 1'?{backgroundColor: '#FFB84C'}:{backgroundColor: '#F16767'}}>
+        {header}
     </div>
   )
 }
