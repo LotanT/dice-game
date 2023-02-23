414d6d6eb0c89f83f4246eb52d9e4afdd0e61821
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-02-23 09:36:12 +0200

    adding dice images create player display component

---
+++ src/cmps/PlayerDisplay/PlayerDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './player-display.css'
+
+const PlayerDisplay = () => {
+  return (
+    <div className='player-display'>
+        player 1
+    </div>
+  )
+}
+
+export default PlayerDisplay
