8be55acded359799c3a72d56533b4ab658b01c86
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2023-02-24 16:53:23 +0200

    more styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import { useState } from 'react';
 import PlayerDisplay from './cmps/PlayerDisplay/PlayerDisplay';
 import './App.css';
+import DICE0 from '../src/assets/imgs/dice-six-faces-0.png'
+import DICE1 from '../src/assets/imgs/dice-six-faces-1.png'
+import DICE2 from '../src/assets/imgs/dice-six-faces-2.png'
+import DICE3 from '../src/assets/imgs/dice-six-faces-3.png'
+import DICE4 from '../src/assets/imgs/dice-six-faces-4.png'
+import DICE5 from '../src/assets/imgs/dice-six-faces-5.png'
+import DICE6 from '../src/assets/imgs/dice-six-faces-6.png'
 
 function App() {
   const [scorePlayer1, setScorePlayer1] = useState(0)
   const [scorePlayer2, setScorePlayer2] = useState(0)
   const [currTurn, setCurrTurn] = useState('player1')
   const [currentScore, setCurrentScore] = useState(0)
-  const [dice, setDice] = useState({dice1: 0, dice2: 0})
+  const [dice, setDice] = useState({dice1: DICE0, dice2: DICE0})
 
   const rollingDice = () =>{
     const dice1 = Math.floor(Math.random() * 6)+1
     const dice2 = Math.floor(Math.random() * 6)+1
-    setDice({dice1, dice2})
+    const dice = {}
+    switch (dice1) {
+      case 1:
+        dice.dice1 = DICE1
+        break;
+      case 2:
+        dice.dice1 = DICE2
+        break;
+      case 3:
+        dice.dice1 = DICE3
+        break;
+      case 4:
+        dice.dice1 = DICE4
+        break;
+      case 5:
+        dice.dice1 = DICE5
+        break;
+      case 6:
+        dice.dice1 = DICE6
+        break;
+    }
+    switch (dice2) {
+      case 1:
+        dice.dice2 = DICE1
+        break;
+      case 2:
+        dice.dice2 = DICE2
+        break;
+      case 3:
+        dice.dice2 = DICE3
+        break;
+      case 4:
+        dice.dice2 = DICE4
+        break;
+      case 5:
+        dice.dice2 = DICE5
+        break;
+      case 6:
+        dice.dice2 = DICE6
+        break;
+    }
+    setDice(dice)
     if(dice1 === 6 && dice2 === 6) switchTurn()
     else setCurrentScore(prevState=>prevState+dice1+dice2)
   }
@@ -39,18 +87,18 @@ function App() {
   console.log(dice, currentScore, scorePlayer1, scorePlayer2);
   return (
     <div className="App">
-     <PlayerDisplay header={'player 2'} score={scorePlayer2} currentScore={currTurn==='player2'?currentScore:0}/>
+     <PlayerDisplay header={'PLAYER  2'} score={scorePlayer2} currentScore={currTurn==='player2'?currentScore:0}/>
      <div className='board'>
-      <div onClick={resetGame}>new game</div>
+      <div onClick={resetGame} className='button'>NEW GAME</div>
       <div>
-        <div className='dice'><img alt='' src={require(`../src/assets/imgs/dice-six-faces-${dice.dice1}.png`)}/></div>
-        <div className='dice'><img alt='' src={require(`../src/assets/imgs/dice-six-faces-${dice.dice2}.png`)}/></div>
+        <div className='dice'><img alt='' src={dice.dice1}/></div>
+        <div className='dice'><img alt='' src={dice.dice2 }/></div>
       </div>
-      <div onClick={rollingDice}>Roll Dice</div>
-      <div onClick={holdPoints}>HOLD</div>
-      <label>Score to win: <input/></label>
+      <div onClick={rollingDice} className='button'>ROLL DICE</div>
+      <div onClick={holdPoints} className='button'>HOLD</div>
+      <label>Score to win: <input placeholder='FINAL SCORE' defaultValue='100'/></label>
      </div>
-     <PlayerDisplay header={'player 1'} score={scorePlayer1} currentScore={currTurn==='player1'?currentScore:0}/>
+     <PlayerDisplay header={'PLAYER 1'} score={scorePlayer1} currentScore={currTurn==='player1'?currentScore:0}/>
     </div>
   );
 }
--- src/cmps/PlayerDisplay/PlayerDisplay.js
+++ src/cmps/PlayerDisplay/PlayerDisplay.js
@@ -3,14 +3,14 @@ import './player-display.css'
 
 const PlayerDisplay = ({header, score = 0, currentScore}) => {
   return (
-    <div className='player-display' style={header==='player 1'?{backgroundColor: '#FFB84C'}:{backgroundColor: '#F16767'}}>
-        <div>{header}</div>
-        <div>
-            <div>Score:</div>
-            <div>{score}</div>
+    <div className='player-display' style={header==='PLAYER 1'?{backgroundColor: '#20262E'}:{backgroundColor: '#20262E'}}>
+        <div className='player-header'>{header}</div>
+        <div className='player-score'>
+            <div>SCORE</div>
+            <div style={{fontSize: '62px'}}>{score}</div>
         </div>
-        <div>
-            <div>Current:</div>
+        <div className='current-score'>
+            <div>CURRENT</div>
             <div>{currentScore}</div>
         </div>
         <div></div>
